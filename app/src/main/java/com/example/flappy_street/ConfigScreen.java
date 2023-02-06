ac5fc9e3d2871f6f15614acc9d99ec42510fca87
mrowe34@gatech.edu <40lbskunk@gmail.com>
2023-02-06 14:33:36 -0500

    Created config screen activity

---
+++ app/src/main/java/com/example/flappy_street/ConfigScreen.java
@@ -0,0 +1,13 @@
+package com.example.flappy_street;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConfigScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
