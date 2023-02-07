ba47948910e0ee8cb1b6208645526ce0c36cad32
mrowe34@gatech.edu <40lbskunk@gmail.com>
2023-02-06 22:55:31 -0500

    styling update

--- app/src/main/java/com/example/flappy_street/ConfigScreen.java
+++ app/src/main/java/com/example/flappy_street/ConfigScreen.java
@@ -7,20 +7,26 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.flappy_street.activities.DifficultyButton;
+import com.example.flappy_street.game.DifficultyLevel;
 import com.example.flappy_street.game.Player;
 
 public class ConfigScreen extends AppCompatActivity {
 
     private Button goButton;
+    private DifficultyLevel difficulty;
     private Player player;
     private EditText textField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_config);
+        this.difficulty = DifficultyLevel.MEDIUM;
     }
 
-    private void onButtonPress(View v) {
-
+    private void difficultyPress(View v) {
+        DifficultyButton button = (DifficultyButton) v;
+        button.setPressed(true);
     }
 }
--- app/src/main/java/com/example/flappy_street/difficultyLevel.java
+++
@@ -1,7 +0,0 @@
-package com.example.flappy_street;
-
-public enum difficultyLevel {
-    EASY,
-    MEDIUM,
-    HARD
-}
