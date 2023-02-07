491c01c601886e236782615ebc72d2f25e6e8902
mrowe34@gatech.edu <40lbskunk@gmail.com>
2023-02-07 00:01:38 -0500

    Button functionality, xml cleanup

--- app/src/main/java/com/example/flappy_street/ConfigScreen.java
+++ app/src/main/java/com/example/flappy_street/ConfigScreen.java
@@ -7,7 +7,7 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.flappy_street.activities.DifficultyButton;
+
 import com.example.flappy_street.game.DifficultyLevel;
 import com.example.flappy_street.game.Player;
 
@@ -15,6 +15,7 @@ public class ConfigScreen extends AppCompatActivity {
 
     private Button goButton;
     private DifficultyLevel difficulty;
+    private Button[] difficultyButtons;
     private Player player;
     private EditText textField;
 
@@ -23,10 +24,39 @@ public class ConfigScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_config);
         this.difficulty = DifficultyLevel.MEDIUM;
+        this.goButton = findViewById(R.id.goButton);
+        this.difficultyButtons[0] = findViewById(R.id.easyButton);
+        this.difficultyButtons[1] = findViewById(R.id.mediumButton);
+        this.difficultyButtons[2] = findViewById(R.id.hardButton);
+        for (Button button : difficultyButtons) {
+            button.setOnClickListener(this::difficultyPress);
+        }
     }
 
-    private void difficultyPress(View v) {
-        DifficultyButton button = (DifficultyButton) v;
-        button.setPressed(true);
+    public void difficultyPress(View v) {
+        int pos = 4;
+        Button pressed = (Button) v;
+        for (int i = 0; i < 3; i++) {
+            if (difficultyButtons[i] == pressed) {
+                pos = i;
+                difficultyButtons[i].setPressed(true);
+            } else {
+                difficultyButtons[i].setPressed(false);
+            }
+        }
+
+        switch(pos) {
+            case 0:
+                difficulty = DifficultyLevel.EASY;
+                break;
+            case 1:
+                difficulty = DifficultyLevel.MEDIUM;
+                break;
+            case 2:
+                difficulty = DifficultyLevel.HARD;
+                break;
+            default:
+                throw new IndexOutOfBoundsException("That button don't exist yo");
+        }
     }
 }
