8b0f088b25227ef41f7d085f90c378ae920035ce
mrowe34@gatech.edu <40lbskunk@gmail.com>
2023-02-07 19:59:43 -0500

    MainActivity style cleanup

--- app/src/main/java/com/example/flappy_street/MainActivity.java
+++ app/src/main/java/com/example/flappy_street/MainActivity.java
@@ -8,20 +8,15 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openConfig();
-            }
-        });
+        Button startButton = findViewById(R.id.button);
+        startButton.setOnClickListener(this::openConfig);
     }
-    public void openConfig() {
+    public void openConfig(View v) {
         Intent config = new Intent(this, ConfigScreen.class);
         startActivity(config);
     }
