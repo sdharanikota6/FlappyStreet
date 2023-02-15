07b3ccbf755a0e3ade0c83b6f9e3476cfc976092
mrowe34@gatech.edu <40lbskunk@gmail.com>
2023-02-14 19:51:21 -0500

    Interactable is now GameTile, an abstract class

---
+++ app/src/main/java/com/example/flappy_street/tiles/GameTile.java
@@ -0,0 +1,39 @@
+package com.example.flappy_street.tiles;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+import com.example.flappy_street.game.Player;
+
+/**
+ * A {@code GameTile} is an {@code ImageView} that the player is able to step on.
+ * When the player's position overlaps with the tile's position, then the function
+ * {@code step(Player)} should be called.
+ */
+public abstract class GameTile extends AppCompatImageView {
+
+    /** If the tile has been previously stepped on or not */
+    protected boolean stepped;
+
+    /**
+     * @see AppCompatImageView#AppCompatImageView(Context, AttributeSet)
+     * @param context The context this tile was created in
+     * @param attrs The attribute set used to create this (XML)
+     */
+    public GameTile(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        stepped = false;
+    }
+
+    /**
+     * When a player steps on this tile, execute this code. Typically, this will require some
+     * modification of the player (movement, damage, score increase, etc.). <p>
+     * Implementation should set "stepped" to true.
+     * @param player the player object that stepped on this tile
+     * @return An instance of the object that was stepped on.
+     */
+    public abstract GameTile step(Player player);
+
+}
