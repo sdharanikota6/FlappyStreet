e65e48dc6f5c86acfb85b085ea3d4f7528c752a1
Holden Casey <me@holdencasey.com>
2023-02-17 13:36:39 -0500

    Removed references to Player, will be handled elsewhere

--- app/src/main/java/com/example/flappy_street/levels/GameLevel.java
+++ app/src/main/java/com/example/flappy_street/levels/GameLevel.java
@@ -10,7 +10,6 @@ import com.example.flappy_street.tiles.GameTile;
 
 public class GameLevel extends ConstraintLayout {
     private GameTile[][] tileArray;
-    private Player player; // Need access to player to set score and change lives
     private String name;
 
     public GameLevel(Context context, AttributeSet attributeSet){
@@ -19,7 +18,6 @@ public class GameLevel extends ConstraintLayout {
 
     public GameLevel init(GameTile[][] tileArray, Player player, String name) {
         this.tileArray = tileArray;
-        this.player = player;
         this.name = name;
         return this;
     }
@@ -30,9 +28,5 @@ public class GameLevel extends ConstraintLayout {
 
     public String getName() {
         return name;
-    }
-
-    public void setPlayer(Player player) {
-        this.player = player;
     }
 }
