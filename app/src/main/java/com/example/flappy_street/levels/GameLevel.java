ca9dccdd1b92d965cfb2fb027e2d9b678411a729
Holden Casey <me@holdencasey.com>
2023-02-17 13:36:08 -0500

    GameLevel extends ConstraintLayout

--- app/src/main/java/com/example/flappy_street/levels/GameLevel.java
+++ app/src/main/java/com/example/flappy_street/levels/GameLevel.java
@@ -1,17 +1,27 @@
 package com.example.flappy_street.levels;
 
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import com.example.flappy_street.game.Player;
 import com.example.flappy_street.tiles.GameTile;
 
-public class GameLevel {
-    private final GameTile[][] tileArray;
+public class GameLevel extends ConstraintLayout {
+    private GameTile[][] tileArray;
     private Player player; // Need access to player to set score and change lives
-    private final String name;
+    private String name;
 
-    public GameLevel(GameTile[][] tileArray, Player player, String name) {
+    public GameLevel(Context context, AttributeSet attributeSet){
+        super(context, attributeSet);
+    }
+
+    public GameLevel init(GameTile[][] tileArray, Player player, String name) {
         this.tileArray = tileArray;
         this.player = player;
         this.name = name;
+        return this;
     }
 
     public GameTile[][] getTileArray() {
