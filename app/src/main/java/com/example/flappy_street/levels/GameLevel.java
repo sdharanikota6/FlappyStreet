97d4644a06b61cccafb998f4b9cf8fae2f4fb57e
Holden Casey <me@holdencasey.com>
2023-02-23 18:54:35 -0500

    GameLevel now extends GridLayout

--- app/src/main/java/com/example/flappy_street/levels/GameLevel.java
+++ app/src/main/java/com/example/flappy_street/levels/GameLevel.java
@@ -2,13 +2,14 @@ package com.example.flappy_street.levels;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.GridLayout;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.flappy_street.game.Player;
 import com.example.flappy_street.tiles.GameTile;
 
-public class GameLevel extends ConstraintLayout {
+public class GameLevel extends GridLayout {
     private GameTile[][] tileArray;
     private String name;
 
