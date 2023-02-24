f5b4c60ee61920ae0e0732cee456cdef42cdbc38
Holden Casey <me@holdencasey.com>
2023-02-23 19:11:46 -0500

    Created a PlayerCanMove unit test

---
+++ app/src/test/java/com/example/flappy_street/PlayerCanMoveUnitTest.java
@@ -0,0 +1,12 @@
+package com.example.flappy_street;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PlayerCanMoveUnitTest{
+    @Test
+    public void canMoveLeft() {
+
+    }
+}
