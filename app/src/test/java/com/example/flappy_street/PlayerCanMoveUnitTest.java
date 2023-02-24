a89c09bcf832f5266ac555177226828115821295
Holden Casey <me@holdencasey.com>
2023-02-23 19:32:38 -0500

    Working on canMoveLeft

--- app/src/test/java/com/example/flappy_street/PlayerCanMoveUnitTest.java
+++ app/src/test/java/com/example/flappy_street/PlayerCanMoveUnitTest.java
@@ -1,12 +1,23 @@
 package com.example.flappy_street;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.example.flappy_street.game.DifficultyLevel;
+import com.example.flappy_street.game.Player;
 
 import org.junit.Test;
 
 public class PlayerCanMoveUnitTest{
     @Test
     public void canMoveLeft() {
-
+        /* Does not compile because I don't know how to pass context and attributes yet */
+//        Player player = new Player("test", DifficultyLevel.EASY);
+//        player.setxPos(10); // Need to auto set in Player constructor I would think
+//        player.setyPos(10); // Need to auto set in Player constructor I would think
+//        int oldPos = player.getxPos();
+//        player.moveLeft();
+//        int newPos = player.getxPos();
+//        assertTrue(newPos < oldPos);
     }
 }
