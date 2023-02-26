65fe96be732d6b0dd6762945c125a3a326c6829c
Gloria <gloria01776@gmail.com>
2023-02-25 19:26:09 -0500

    safeTile test

--- app/src/test/java/com/example/flappy_street/SafeTileTest.java
+++ app/src/test/java/com/example/flappy_street/SafeTileTest.java
@@ -9,19 +9,20 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Tests whether or not the player loses lives from stepping on a safe tile.
+ * Tests whether or not the player loses lives from stepping on a safe tile.q
  */
 
 public class SafeTileTest {
 
     @Test
     public void stepOnSafeTile() {
-        SafeTile safeTile = new SafeTile();
+        SafeTile safeTile = new SafeTile(null);
         Player testPlayer = new Player("Robert", DifficultyLevel.EASY);
         int before = testPlayer.getLives();
         if (safeTile.step(testPlayer) == safeTile) {
             assertEquals(before, testPlayer.getLives());
         }
     }
+
 
 }
