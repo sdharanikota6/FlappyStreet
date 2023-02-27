799a384d7d8807e14ffdcc97aae52b840e4c65ec
mrowe34@gatech.edu <40lbskunk@gmail.com>
2023-02-27 14:18:59 -0500

    New abstract class to represent vehicles

---
+++ app/src/main/java/com/example/flappy_street/obstacles/Vehicle.java
@@ -0,0 +1,27 @@
+package com.example.flappy_street.obstacles;
+
+import android.content.Context;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+public abstract class Vehicle extends AppCompatImageView {
+
+    private int xPos;
+    private int yPos;
+    private int size;
+
+    /**
+     * Create a new vehicle.
+     * Subclasses should add a starting x position and add an image to represent it.
+     * @param context The context this vehicle was created in
+     */
+    public Vehicle(Context context) {
+        super(context);
+    }
+
+    /**
+     * Move the vehicle in a direction. Implementation should handle backend movement,
+     * specify the direction, and collision detection.
+     */
+    public abstract void move();
+}
