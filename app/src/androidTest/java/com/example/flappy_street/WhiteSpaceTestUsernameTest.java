c3499d903b69ebe35f52f27d75f7d5ead352e350
Holden Casey <me@holdencasey.com>
2023-03-11 22:55:55 -0500

    Fixed and Refactored WhiteSpaceTestUsernameTest

--- app/src/androidTest/java/com/example/flappy_street/WhiteSpaceTestUsernameTest.java
+++ app/src/androidTest/java/com/example/flappy_street/WhiteSpaceTestUsernameTest.java
@@ -1,5 +1,7 @@
 package com.example.flappy_street;
 
+import androidx.test.core.app.ApplicationProvider;
+
 import com.example.flappy_street.game.DifficultyLevel;
 import com.example.flappy_street.game.Player;
 
@@ -13,12 +15,14 @@ public class WhiteSpaceTestUsernameTest {
     @Test (expected = IllegalArgumentException.class)
     public void inputEmpty() {
         String name = "";
-        Player noNamePlayer = new Player(name, DifficultyLevel.EASY);
+        Player noNamePlayer = new Player(ApplicationProvider.getApplicationContext());
+        noNamePlayer.init(1, name, DifficultyLevel.EASY);
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void inputSpace() {
         String name = "    ";
-        Player noNamePlayer = new Player(name, DifficultyLevel.EASY);
+        Player noNamePlayer = new Player(ApplicationProvider.getApplicationContext());
+        noNamePlayer.init(1, name, DifficultyLevel.EASY);
     }
 }
