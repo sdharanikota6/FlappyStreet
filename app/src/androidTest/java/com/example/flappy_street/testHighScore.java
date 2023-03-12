9b1b734dd49dff81141db381dc045f97be956e81
Holden Casey <me@holdencasey.com>
2023-03-11 22:57:44 -0500

    Fixed and Refactored testHighScore

--- app/src/androidTest/java/com/example/flappy_street/testHighScore.java
+++ app/src/androidTest/java/com/example/flappy_street/testHighScore.java
@@ -2,6 +2,8 @@ package com.example.flappy_street;
 
 import static org.junit.Assert.assertEquals;
 
+import androidx.test.core.app.ApplicationProvider;
+
 import com.example.flappy_street.game.DifficultyLevel;
 import com.example.flappy_street.game.Player;
 
@@ -10,7 +12,8 @@ import org.junit.Test;
 public class testHighScore {
 
     public void highScore() {
-        Player player = new Player("Sudeep", DifficultyLevel.EASY);
+        Player player = new Player(ApplicationProvider.getApplicationContext());
+        player.init(1, "Sudeep", DifficultyLevel.EASY);
         player.win();
         assertEquals(100, player.getHighScore());
     }
