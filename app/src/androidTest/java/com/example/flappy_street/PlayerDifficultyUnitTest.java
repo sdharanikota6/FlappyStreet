c33ca267c12fa147541bf9575b22935649891912
Holden Casey <me@holdencasey.com>
2023-03-14 09:35:11 -0400

    More runtime fixes to all unit tests. All in android should pass now

--- app/src/androidTest/java/com/example/flappy_street/PlayerDifficultyUnitTest.java
+++ app/src/androidTest/java/com/example/flappy_street/PlayerDifficultyUnitTest.java
@@ -27,7 +27,7 @@ public class PlayerDifficultyUnitTest {
         DifficultyLevel medium = DifficultyLevel.MEDIUM;
         String name = "Test";
         Player mediumPlayer = new Player(ApplicationProvider.getApplicationContext());
-        mediumPlayer.init(1, name, medium);
+        mediumPlayer.init(0, name, medium);
         assertEquals(3, mediumPlayer.getLives());
     }
 
@@ -36,7 +36,7 @@ public class PlayerDifficultyUnitTest {
         DifficultyLevel hard = DifficultyLevel.HARD;
         String name = "Test";
         Player hardPlayer = new Player(ApplicationProvider.getApplicationContext());
-        hardPlayer.init(1, name, hard);
+        hardPlayer.init(0, name, hard);
         assertEquals(1, hardPlayer.getLives());
     }
 }
