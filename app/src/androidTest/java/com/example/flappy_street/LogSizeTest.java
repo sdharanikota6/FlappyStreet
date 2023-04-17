ea95f1e716133cdae3b58d62ad2c066db99667a6
mrowe34@gatech.edu <40lbskunk@gmail.com>
2023-04-16 22:54:17 -0400

    test fixes

--- app/src/androidTest/java/com/example/flappy_street/LogSizeTest.java
+++ app/src/androidTest/java/com/example/flappy_street/LogSizeTest.java
@@ -1,16 +1,13 @@
 package com.example.flappy_street;
 
-import static org.junit.Assert.assertNotEquals;
-
 import android.content.Context;
 
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.platform.app.InstrumentationRegistry;
 
-import com.example.flappy_street.obstacles.Car;
-import com.example.flappy_street.obstacles.Semi;
-import com.example.flappy_street.obstacles.Truck;
-import com.example.flappy_street.obstacles.Vehicle;
+import com.example.flappy_street.levels.GameLevel;
+import com.example.flappy_street.obstacles.river.BigPlatform;
+import com.example.flappy_street.obstacles.river.SmallPlatform;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,15 +18,20 @@ public class LogSizeTest {
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
         BigPlatform longLog = new BigPlatform(ApplicationProvider.getApplicationContext());
         SmallPlatform smallLog = new SmallPlatform(ApplicationProvider.getApplicationContext());
-        int beforePosLong = longLog.getX();
-        int beforePosSmall = smallLog.getX();
 
+        GameLevel level = new GameLevel(context);
+        longLog.setGameLevel(level);
+        smallLog.setGameLevel(level);
+        int beforePosLong = longLog.getXPos();
+        int beforePosSmall = smallLog.getXPos();
+        longLog.setYPos(2);
+        smallLog.setYPos(2);
         for (int i = 0; i < 2500; i++) {
             longLog.move();
             smallLog.move();
         }
-        int afterPosLong = longLog.getX();
-        int afterPosSmall = smallLog.getX();;
+        int afterPosLong = longLog.getXPos();
+        int afterPosSmall = smallLog.getXPos();
 
         Assert.assertTrue(afterPosLong > beforePosLong);
         Assert.assertTrue(afterPosSmall > beforePosSmall);
